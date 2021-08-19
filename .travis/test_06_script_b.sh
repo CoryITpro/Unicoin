39fccb0122aad37b36ef0ec78d161ab1a330c5fb
DevShine <80782509+devshine2015@users.noreply.github.com>
2021-08-19 10:45:12 +0300

    unicoin core

--- .travis/test_06_script_b.sh
+++ .travis/test_06_script_b.sh
@@ -6,7 +6,7 @@
 
 export LC_ALL=C.UTF-8
 
-cd "build/litecoin-$HOST" || (echo "could not enter distdir build/litecoin-$HOST"; exit 1)
+cd "build/unicoin-$HOST" || (echo "could not enter distdir build/unicoin-$HOST"; exit 1)
 
 if [ "$RUN_UNIT_TESTS" = "true" ]; then
   BEGIN_FOLD unit-tests
